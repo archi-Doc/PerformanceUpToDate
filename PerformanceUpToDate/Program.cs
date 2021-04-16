5a42fab980dd5c1d3bb43f0aec1cf57e3fbe6b1f
archi-Doc <archidoc422@gmail.com>
2021-04-16 19:15:20 +0900

    Add FillIntArrayTest

--- PerformanceUpToDate/Program.cs
+++ PerformanceUpToDate/Program.cs
@@ -23,12 +23,13 @@ namespace PerformanceUpToDate
     {
         public static void Main(string[] args)
         {
-            DebugRun<SortComparerTest>();
+            DebugRun<FillIntArrayTest>();
 
             // var summary = BenchmarkRunner.Run<ByteCopyTest>(); // SwapTest, MemoryAllocationTest, ByteCopyTest
             var switcher = new BenchmarkSwitcher(new[]
 #pragma warning restore SA1515 // Single-line comment should be preceded by blank line
             {
+                typeof(FillIntArrayTest),
                 typeof(SortComparerTest),
                 typeof(IndexOfTest),
                 typeof(BitTest.NLZ),
