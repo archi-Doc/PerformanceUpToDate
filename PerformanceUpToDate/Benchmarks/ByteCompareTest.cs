6c66535c38443f075e422f50e17bbdc3fc06233b
archi-Doc <archidoc422@gmail.com>
2022-09-03 20:47:41 +0900

    .

--- PerformanceUpToDate/Benchmarks/ByteCompareTest.cs
+++ PerformanceUpToDate/Benchmarks/ByteCompareTest.cs
@@ -11,74 +11,268 @@ using BenchmarkDotNet.Attributes;
 
 #pragma warning disable SA1204 // Static elements should appear before instance elements
 
-namespace PerformanceUpToDate
+namespace PerformanceUpToDate;
+
+[Config(typeof(BenchmarkConfig))]
+public class ByteCompareTest
 {
-    [Config(typeof(BenchmarkConfig))]
-    public class ByteCompareTest
+    private byte[] source;
+    private byte[] destination;
+
+    public ByteCompareTest()
     {
-        private byte[] source;
-        private byte[] destination;
+        this.source = null!;
+        this.destination = null!;
 
-        public ByteCompareTest()
+        this.Setup();
+    }
+
+    public ByteCompareTest(int size)
+    {
+        this.source = null!;
+        this.destination = null!;
+
+        this.Size = size;
+        this.Setup();
+    }
+
+    // [Params(10, 32, 256, 1024, 1_000_000)]
+    [Params(10,  256, 2048)]
+    public int Size { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        this.source = new byte[this.Size];
+        this.destination = new byte[this.Size];
+
+        new Random(42).NextBytes(this.source);
+        Array.Copy(this.source, 0, this.destination, 0, this.source.Length);
+    }
+
+    [Benchmark]
+    public bool ByteCompare_IStructuralEquatable()
+    {
+        return (this.source as IStructuralEquatable).Equals(this.destination, StructuralComparisons.StructuralEqualityComparer);
+    }
+
+    [Benchmark]
+    public bool ByteCompare_SequenceEqual()
+    {
+        return Enumerable.SequenceEqual(this.source, this.destination);
+    }
+
+    [Benchmark]
+    public bool ByteCompare_SequenceEqualSpan()
+    {// Recommended
+        return this.source.AsSpan().SequenceEqual(this.destination);
+    }
+
+    [Benchmark]
+    public bool ByteCompare_For()
+    {
+        if (this.source.Length != this.destination.Length)
         {
-            this.source = null!;
-            this.destination = null!;
-
-            this.Setup();
+            return false;
         }
 
-        public ByteCompareTest(int size)
+        for (int i = 0; i < this.source.Length; i++)
         {
-            this.source = null!;
-            this.destination = null!;
-
-            this.Size = size;
-            this.Setup();
-        }
-
-        // [Params(10, 32, 256, 1024, 1_000_000)]
-        [Params(10,  256, 2048)]
-        public int Size { get; set; }
-
-        [GlobalSetup]
-        public void Setup()
-        {
-            this.source = new byte[this.Size];
-            this.destination = new byte[this.Size];
-
-            new Random(42).NextBytes(this.source);
-            Array.Copy(this.source, 0, this.destination, 0, this.source.Length);
-        }
-
-        [Benchmark]
-        public bool ByteCompare_IStructuralEquatable()
-        {
-            return (this.source as IStructuralEquatable).Equals(this.destination, StructuralComparisons.StructuralEqualityComparer);
-        }
-
-        [Benchmark]
-        public bool ByteCompare_SequenceEqual()
-        {
-            return Enumerable.SequenceEqual(this.source, this.destination);
-        }
-
-        [Benchmark]
-        public bool ByteCompare_SequenceEqualSpan()
-        {// Recommended
-            return this.source.AsSpan().SequenceEqual(this.destination);
-        }
-
-        [Benchmark]
-        public bool ByteCompare_For()
-        {
-            if (this.source.Length != this.destination.Length)
+            if (this.source[i] != this.destination[i])
             {
                 return false;
             }
+        }
 
-            for (int i = 0; i < this.source.Length; i++)
+        return true;
+    }
+
+    [Benchmark]
+    public unsafe bool ByteCompare_int()
+    {
+        if (this.source.Length != this.destination.Length)
+        {
+            return false;
+        }
+
+        int len = this.source.Length;
+        unsafe
+        {
+            fixed (byte* ap = this.source, bp = this.destination)
             {
-                if (this.source[i] != this.destination[i])
+                int* aip = (int*)ap, bip = (int*)bp;
+                for (; len >= 4; len -= 4)
+                {
+                    if (*aip != *bip)
+                    {
+                        return false;
+                    }
+
+                    aip++;
+                    bip++;
+                }
+
+                byte* ap2 = (byte*)aip, bp2 = (byte*)bip;
+                for (; len > 0; len--)
+                {
+                    if (*ap2 != *bp2)
+                    {
+                        return false;
+                    }
+
+                    ap2++;
+                    bp2++;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    [Benchmark]
+    public unsafe bool ByteCompare_int2()
+    {
+        if (this.source == this.destination)
+        {
+            return true;
+        }
+
+        if (this.source == null || this.destination == null)
+        {
+            return false;
+        }
+
+        if (this.source.Length != this.destination.Length)
+        {
+            return false;
+        }
+
+        int len = this.source.Length;
+        fixed (byte* p1 = this.source, p2 = this.destination)
+        {
+            int* i1 = (int*)p1;
+            int* i2 = (int*)p2;
+            while (len >= 4)
+            {
+                if (*i1 != *i2)
+                {
+                    return false;
+                }
+
+                i1++;
+                i2++;
+                len -= 4;
+            }
+
+            byte* c1 = (byte*)i1;
+            byte* c2 = (byte*)i2;
+            while (len > 0)
+            {
+                if (*c1 != *c2)
+                {
+                    return false;
+                }
+
+                c1++;
+                c2++;
+                len--;
+            }
+        }
+
+        return true;
+    }
+
+    [Benchmark]
+    public unsafe bool ByteCompare_int10()
+    {
+        int length = this.source.Length;
+        if (length != this.destination.Length)
+        {
+            return false;
+        }
+
+        fixed (byte* str = this.source)
+        {
+            byte* chPtr = str;
+            fixed (byte* str2 = this.destination)
+            {
+                byte* chPtr2 = str2;
+                byte* chPtr3 = chPtr;
+                byte* chPtr4 = chPtr2;
+                while (length >= 10)
+                {
+                    if ((((*((int*)chPtr3) != *((int*)chPtr4)) || (*((int*)(chPtr3 + 2)) != *((int*)(chPtr4 + 2)))) || ((*((int*)(chPtr3 + 4)) != *((int*)(chPtr4 + 4))) || (*((int*)(chPtr3 + 6)) != *((int*)(chPtr4 + 6))))) || (*((int*)(chPtr3 + 8)) != *((int*)(chPtr4 + 8))))
+                    {
+                        break;
+                    }
+
+                    chPtr3 += 10;
+                    chPtr4 += 10;
+                    length -= 10;
+                }
+
+                while (length > 0)
+                {
+                    if (*((int*)chPtr3) != *((int*)chPtr4))
+                    {
+                        break;
+                    }
+
+                    chPtr3 += 2;
+                    chPtr4 += 2;
+                    length -= 2;
+                }
+
+                return length <= 0;
+            }
+        }
+    }
+
+    [Benchmark]
+    public unsafe bool ByteCompare_intshortbyte()
+    {
+        if (this.source == null || this.destination == null || this.source.Length != this.destination.Length)
+        {
+            return false;
+        }
+
+        fixed (byte* p1 = this.source, p2 = this.destination)
+        {
+            byte* x1 = p1, x2 = p2;
+            int l = this.source.Length;
+            for (int i = 0; i < l / 8; i++, x1 += 8, x2 += 8)
+            {
+                if (*((long*)x1) != *((long*)x2))
+                {
+                    return false;
+                }
+            }
+
+            if ((l & 4) != 0)
+            {
+                if (*((int*)x1) != *((int*)x2))
+                {
+                    return false;
+                }
+
+                x1 += 4;
+                x2 += 4;
+            }
+
+            if ((l & 2) != 0)
+            {
+                if (*((short*)x1) != *((short*)x2))
+                {
+                    return false;
+                }
+
+                x1 += 2;
+                x2 += 2;
+            }
+
+            if ((l & 1) != 0)
+            {
+                if (*((byte*)x1) != *((byte*)x2))
                 {
                     return false;
                 }
@@ -86,266 +280,71 @@ namespace PerformanceUpToDate
 
             return true;
         }
+    }
 
-        [Benchmark]
-        public unsafe bool ByteCompare_int()
+    [Benchmark]
+    public unsafe bool ByteCompare_long()
+    {
+        if (this.source.Length != this.destination.Length)
         {
-            if (this.source.Length != this.destination.Length)
-            {
-                return false;
-            }
-
-            int len = this.source.Length;
-            unsafe
-            {
-                fixed (byte* ap = this.source, bp = this.destination)
-                {
-                    int* aip = (int*)ap, bip = (int*)bp;
-                    for (; len >= 4; len -= 4)
-                    {
-                        if (*aip != *bip)
-                        {
-                            return false;
-                        }
-
-                        aip++;
-                        bip++;
-                    }
-
-                    byte* ap2 = (byte*)aip, bp2 = (byte*)bip;
-                    for (; len > 0; len--)
-                    {
-                        if (*ap2 != *bp2)
-                        {
-                            return false;
-                        }
-
-                        ap2++;
-                        bp2++;
-                    }
-                }
-            }
-
-            return true;
+            return false;
         }
 
-        [Benchmark]
-        public unsafe bool ByteCompare_int2()
+        int len = this.source.Length;
+        unsafe
         {
-            if (this.source == this.destination)
+            fixed (byte* ap = this.source, bp = this.destination)
             {
-                return true;
-            }
-
-            if (this.source == null || this.destination == null)
-            {
-                return false;
-            }
-
-            if (this.source.Length != this.destination.Length)
-            {
-                return false;
-            }
-
-            int len = this.source.Length;
-            fixed (byte* p1 = this.source, p2 = this.destination)
-            {
-                int* i1 = (int*)p1;
-                int* i2 = (int*)p2;
-                while (len >= 4)
+                long* alp = (long*)ap, blp = (long*)bp;
+                for (; len >= 8; len -= 8)
                 {
-                    if (*i1 != *i2)
+                    if (*alp != *blp)
                     {
                         return false;
                     }
 
-                    i1++;
-                    i2++;
-                    len -= 4;
+                    alp++;
+                    blp++;
                 }
 
-                byte* c1 = (byte*)i1;
-                byte* c2 = (byte*)i2;
-                while (len > 0)
+                byte* ap2 = (byte*)alp, bp2 = (byte*)blp;
+                for (; len > 0; len--)
                 {
-                    if (*c1 != *c2)
+                    if (*ap2 != *bp2)
                     {
                         return false;
                     }
 
-                    c1++;
-                    c2++;
-                    len--;
-                }
-            }
-
-            return true;
-        }
-
-        [Benchmark]
-        public unsafe bool ByteCompare_int10()
-        {
-            int length = this.source.Length;
-            if (length != this.destination.Length)
-            {
-                return false;
-            }
-
-            fixed (byte* str = this.source)
-            {
-                byte* chPtr = str;
-                fixed (byte* str2 = this.destination)
-                {
-                    byte* chPtr2 = str2;
-                    byte* chPtr3 = chPtr;
-                    byte* chPtr4 = chPtr2;
-                    while (length >= 10)
-                    {
-                        if ((((*((int*)chPtr3) != *((int*)chPtr4)) || (*((int*)(chPtr3 + 2)) != *((int*)(chPtr4 + 2)))) || ((*((int*)(chPtr3 + 4)) != *((int*)(chPtr4 + 4))) || (*((int*)(chPtr3 + 6)) != *((int*)(chPtr4 + 6))))) || (*((int*)(chPtr3 + 8)) != *((int*)(chPtr4 + 8))))
-                        {
-                            break;
-                        }
-
-                        chPtr3 += 10;
-                        chPtr4 += 10;
-                        length -= 10;
-                    }
-
-                    while (length > 0)
-                    {
-                        if (*((int*)chPtr3) != *((int*)chPtr4))
-                        {
-                            break;
-                        }
-
-                        chPtr3 += 2;
-                        chPtr4 += 2;
-                        length -= 2;
-                    }
-
-                    return length <= 0;
+                    ap2++;
+                    bp2++;
                 }
             }
         }
 
-        [Benchmark]
-        public unsafe bool ByteCompare_intshortbyte()
-        {
-            if (this.source == null || this.destination == null || this.source.Length != this.destination.Length)
-            {
-                return false;
-            }
+        return true;
+    }
 
-            fixed (byte* p1 = this.source, p2 = this.destination)
-            {
-                byte* x1 = p1, x2 = p2;
-                int l = this.source.Length;
-                for (int i = 0; i < l / 8; i++, x1 += 8, x2 += 8)
-                {
-                    if (*((long*)x1) != *((long*)x2))
-                    {
-                        return false;
-                    }
-                }
-
-                if ((l & 4) != 0)
-                {
-                    if (*((int*)x1) != *((int*)x2))
-                    {
-                        return false;
-                    }
-
-                    x1 += 4;
-                    x2 += 4;
-                }
-
-                if ((l & 2) != 0)
-                {
-                    if (*((short*)x1) != *((short*)x2))
-                    {
-                        return false;
-                    }
-
-                    x1 += 2;
-                    x2 += 2;
-                }
-
-                if ((l & 1) != 0)
-                {
-                    if (*((byte*)x1) != *((byte*)x2))
-                    {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-        }
-
-        [Benchmark]
-        public unsafe bool ByteCompare_long()
-        {
-            if (this.source.Length != this.destination.Length)
-            {
-                return false;
-            }
-
-            int len = this.source.Length;
-            unsafe
-            {
-                fixed (byte* ap = this.source, bp = this.destination)
-                {
-                    long* alp = (long*)ap, blp = (long*)bp;
-                    for (; len >= 8; len -= 8)
-                    {
-                        if (*alp != *blp)
-                        {
-                            return false;
-                        }
-
-                        alp++;
-                        blp++;
-                    }
-
-                    byte* ap2 = (byte*)alp, bp2 = (byte*)blp;
-                    for (; len > 0; len--)
-                    {
-                        if (*ap2 != *bp2)
-                        {
-                            return false;
-                        }
-
-                        ap2++;
-                        bp2++;
-                    }
-                }
-            }
-
-            return true;
-        }
-
-        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
 #pragma warning disable SA1300 // Element should begin with upper-case letter
-        public static extern int memcmp(byte[] b1, byte[] b2, long count);
+    public static extern int memcmp(byte[] b1, byte[] b2, long count);
 #pragma warning restore SA1300 // Element should begin with upper-case letter
 
-        [Benchmark]
-        public bool ByteCompare_memcmp()
-        {
-            return this.source.Length == this.destination.Length && memcmp(this.source, this.destination, this.source.Length) == 0;
-        }
+    [Benchmark]
+    public bool ByteCompare_memcmp()
+    {
+        return this.source.Length == this.destination.Length && memcmp(this.source, this.destination, this.source.Length) == 0;
+    }
 
-        public void Test()
-        {
-            Debug.Assert(this.ByteCompare_IStructuralEquatable(), "ByteCompare_IStructuralEquatable");
-            Debug.Assert(this.ByteCompare_SequenceEqual(), "ByteCompare_SequenceEqual");
-            Debug.Assert(this.ByteCompare_For(), "ByteCompare_For");
-            Debug.Assert(this.ByteCompare_int(), "ByteCompare_int");
-            Debug.Assert(this.ByteCompare_int2(), "ByteCompare_int2");
-            Debug.Assert(this.ByteCompare_int10(), "ByteCompare_int10");
-            Debug.Assert(this.ByteCompare_intshortbyte(), "ByteCompare_intshortbyte");
-            Debug.Assert(this.ByteCompare_long(), "ByteCompare_long");
-            Debug.Assert(this.ByteCompare_memcmp(), "ByteCompare_memcmp");
-        }
+    public void Test()
+    {
+        Debug.Assert(this.ByteCompare_IStructuralEquatable(), "ByteCompare_IStructuralEquatable");
+        Debug.Assert(this.ByteCompare_SequenceEqual(), "ByteCompare_SequenceEqual");
+        Debug.Assert(this.ByteCompare_For(), "ByteCompare_For");
+        Debug.Assert(this.ByteCompare_int(), "ByteCompare_int");
+        Debug.Assert(this.ByteCompare_int2(), "ByteCompare_int2");
+        Debug.Assert(this.ByteCompare_int10(), "ByteCompare_int10");
+        Debug.Assert(this.ByteCompare_intshortbyte(), "ByteCompare_intshortbyte");
+        Debug.Assert(this.ByteCompare_long(), "ByteCompare_long");
+        Debug.Assert(this.ByteCompare_memcmp(), "ByteCompare_memcmp");
     }
 }
--- PerformanceUpToDate/Program.cs
+++ PerformanceUpToDate/Program.cs
@@ -17,119 +17,119 @@ using BenchmarkDotNet.Running;
 
 #pragma warning disable SA1515 // Single-line comment should be preceded by blank line
 
-namespace PerformanceUpToDate
+namespace PerformanceUpToDate;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            DebugRun<DynamicAccessTest>();
+        DebugRun<DynamicAccessTest>();
 
-            // var summary = BenchmarkRunner.Run<ByteCopyTest>(); // SwapTest, MemoryAllocationTest, ByteCopyTest
-            var switcher = new BenchmarkSwitcher(new[]
+        // var summary = BenchmarkRunner.Run<ByteCopyTest>(); // SwapTest, MemoryAllocationTest, ByteCopyTest
+        var switcher = new BenchmarkSwitcher(new[]
 #pragma warning restore SA1515 // Single-line comment should be preceded by blank line
-            {
-                typeof(DynamicAccessTest),
-                typeof(AsyncEventTest),
-                typeof(AsyncLocalTest),
-                typeof(TaskTest),
-                typeof(TimerTest),
-                typeof(SyncDesignTest),
-                typeof(ByteToULongTest),
-                typeof(FillByteArrayTest),
-                typeof(CopyIntArrayTest),
-                typeof(FillIntArrayTest),
-                typeof(SortComparerTest),
-                typeof(IndexOfTest),
-                typeof(BitTest.NLZ),
-                typeof(RefTest.RefTest1),
-                typeof(RefTest.RefTest2),
-                typeof(RefTest.RefTest3),
-                typeof(NewInstance.NewInstanceTest),
-                typeof(StructTest),
-                typeof(DelegateTest),
-                typeof(MemoryAllocationTest),
-                typeof(ByteCopyTest),
-                typeof(ByteCompareTest),
-                typeof(SpanTest),
-                typeof(StreamTest),
-                typeof(ImmutableTest),
-                typeof(StringTest),
-            });
-            switcher.Run(args);
-        }
-
-        public static void DebugRun<T>()
-            where T : new()
-        { // Run a benchmark in debug mode.
-            var t = new T();
-            var type = typeof(T);
-            var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance);
-            var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
-            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-            foreach (var x in fields)
-            { // Set Fields.
-                var attr = (ParamsAttribute[])x.GetCustomAttributes(typeof(ParamsAttribute), false);
-                if (attr != null && attr.Length > 0)
-                {
-                    if (attr[0].Values.Length > 0)
-                    {
-                        x.SetValue(t, attr[0].Values[0]);
-                    }
-                }
-            }
-
-            foreach (var x in properties)
-            { // Set Properties.
-                var attr = (ParamsAttribute[])x.GetCustomAttributes(typeof(ParamsAttribute), false);
-                if (attr != null && attr.Length > 0)
-                {
-                    if (attr[0].Values.Length > 0)
-                    {
-                        x.SetValue(t, attr[0].Values[0]);
-                    }
-                }
-            }
-
-            foreach (var x in methods.Where(i => i.GetCustomAttributes(typeof(GlobalSetupAttribute), false).Length > 0))
-            { // [GlobalSetupAttribute]
-                x.Invoke(t, null);
-            }
-
-            foreach (var x in methods.Where(i => i.GetCustomAttributes(typeof(BenchmarkAttribute), false).Length > 0))
-            { // [BenchmarkAttribute]
-                x.Invoke(t, null);
-            }
-
-            foreach (var x in methods.Where(i => i.GetCustomAttributes(typeof(GlobalCleanupAttribute), false).Length > 0))
-            { // [GlobalCleanupAttribute]
-                x.Invoke(t, null);
-            }
-
-            // obsolete code:
-            // methods.Where(i => i.CustomAttributes.Select(j => j.AttributeType).Contains(typeof(GlobalSetupAttribute)))
-            // bool IsNullableType(Type type) => type.IsGenericType && type.GetGenericTypeDefinition().Equals(typeof(Nullable<>));
-            /* var targetType = IsNullableType(x.FieldType) ? Nullable.GetUnderlyingType(x.FieldType) : x.FieldType;
-                        if (targetType != null)
-                        {
-                            var value = Convert.ChangeType(attr[0].Values[0], targetType);
-                            x.SetValue(t, value);
-                        }*/
-        }
+        {
+            typeof(DynamicAccessTest),
+            typeof(AsyncEventTest),
+            typeof(AsyncLocalTest),
+            typeof(TaskTest),
+            typeof(TimerTest),
+            typeof(SyncDesignTest),
+            typeof(ByteToULongTest),
+            typeof(FillByteArrayTest),
+            typeof(CopyIntArrayTest),
+            typeof(FillIntArrayTest),
+            typeof(SortComparerTest),
+            typeof(IndexOfTest),
+            typeof(BitTest.NLZ),
+            typeof(RefTest.RefTest1),
+            typeof(RefTest.RefTest2),
+            typeof(RefTest.RefTest3),
+            typeof(NewInstance.NewInstanceTest),
+            typeof(StructTest),
+            typeof(DelegateTest),
+            typeof(MemoryAllocationTest),
+            typeof(ByteCopyTest),
+            typeof(ByteCompareTest),
+            typeof(ByteCompareTest2),
+            typeof(SpanTest),
+            typeof(StreamTest),
+            typeof(ImmutableTest),
+            typeof(StringTest),
+        });
+        switcher.Run(args);
     }
 
-    public class BenchmarkConfig : BenchmarkDotNet.Configs.ManualConfig
-    {
-        public BenchmarkConfig()
-        {
-            this.AddExporter(BenchmarkDotNet.Exporters.MarkdownExporter.GitHub);
-            this.AddDiagnoser(BenchmarkDotNet.Diagnosers.MemoryDiagnoser.Default);
+    public static void DebugRun<T>()
+        where T : new()
+    { // Run a benchmark in debug mode.
+        var t = new T();
+        var type = typeof(T);
+        var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+        var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
+        var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
-            // this.Add(Job.ShortRun.With(BenchmarkDotNet.Environments.Platform.X64).WithWarmupCount(1).WithIterationCount(1));
-            // this.Add(BenchmarkDotNet.Jobs.Job.MediumRun.WithGcForce(true).WithId("GcForce medium"));
-            // this.AddJob(BenchmarkDotNet.Jobs.Job.ShortRun);
-            this.AddJob(BenchmarkDotNet.Jobs.Job.MediumRun);
+        foreach (var x in fields)
+        { // Set Fields.
+            var attr = (ParamsAttribute[])x.GetCustomAttributes(typeof(ParamsAttribute), false);
+            if (attr != null && attr.Length > 0)
+            {
+                if (attr[0].Values.Length > 0)
+                {
+                    x.SetValue(t, attr[0].Values[0]);
+                }
+            }
         }
+
+        foreach (var x in properties)
+        { // Set Properties.
+            var attr = (ParamsAttribute[])x.GetCustomAttributes(typeof(ParamsAttribute), false);
+            if (attr != null && attr.Length > 0)
+            {
+                if (attr[0].Values.Length > 0)
+                {
+                    x.SetValue(t, attr[0].Values[0]);
+                }
+            }
+        }
+
+        foreach (var x in methods.Where(i => i.GetCustomAttributes(typeof(GlobalSetupAttribute), false).Length > 0))
+        { // [GlobalSetupAttribute]
+            x.Invoke(t, null);
+        }
+
+        foreach (var x in methods.Where(i => i.GetCustomAttributes(typeof(BenchmarkAttribute), false).Length > 0))
+        { // [BenchmarkAttribute]
+            x.Invoke(t, null);
+        }
+
+        foreach (var x in methods.Where(i => i.GetCustomAttributes(typeof(GlobalCleanupAttribute), false).Length > 0))
+        { // [GlobalCleanupAttribute]
+            x.Invoke(t, null);
+        }
+
+        // obsolete code:
+        // methods.Where(i => i.CustomAttributes.Select(j => j.AttributeType).Contains(typeof(GlobalSetupAttribute)))
+        // bool IsNullableType(Type type) => type.IsGenericType && type.GetGenericTypeDefinition().Equals(typeof(Nullable<>));
+        /* var targetType = IsNullableType(x.FieldType) ? Nullable.GetUnderlyingType(x.FieldType) : x.FieldType;
+                    if (targetType != null)
+                    {
+                        var value = Convert.ChangeType(attr[0].Values[0], targetType);
+                        x.SetValue(t, value);
+                    }*/
+    }
+}
+
+public class BenchmarkConfig : BenchmarkDotNet.Configs.ManualConfig
+{
+    public BenchmarkConfig()
+    {
+        this.AddExporter(BenchmarkDotNet.Exporters.MarkdownExporter.GitHub);
+        this.AddDiagnoser(BenchmarkDotNet.Diagnosers.MemoryDiagnoser.Default);
+
+        // this.Add(Job.ShortRun.With(BenchmarkDotNet.Environments.Platform.X64).WithWarmupCount(1).WithIterationCount(1));
+        // this.Add(BenchmarkDotNet.Jobs.Job.MediumRun.WithGcForce(true).WithId("GcForce medium"));
+        // this.AddJob(BenchmarkDotNet.Jobs.Job.ShortRun);
+        this.AddJob(BenchmarkDotNet.Jobs.Job.MediumRun);
     }
 }
