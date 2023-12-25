6275454947ba221216d74524b24a36fce4f5bb39
archi-Doc <archidoc422@gmail.com>
2023-12-25 23:31:31 +0900

    ...

---
+++ PerformanceUpToDate/Benchmarks/CancellationTokenTest.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Arc.Collections;
+using BenchmarkDotNet.Attributes;
+
+namespace PerformanceUpToDate;
+
+[Config(typeof(BenchmarkConfig))]
+public class CancellationTokenTest
+{
+    private int count;
+    private CancellationTokenSource cts;
+    private CancellationToken cancellationToken;
+    private ObjectPool<CancellationTokenSource> ctsPool = new(() => new());
+
+    public CancellationTokenTest()
+    {
+        this.cts = new();
+        this.cancellationToken = cts.Token;
+    }
+
+    private async Task<int> TestTask()
+    {
+        return this.count++;
+        // return Task.Delay(100);
+    }
+
+    private async Task<int> TestTask(CancellationToken cancellationToken)
+    {
+        return this.count++;
+        // return Task.Delay(100, cancellationToken);
+    }
+
+    [Benchmark]
+    public int Task_WaitAsync()
+    {
+        var task = this.TestTask().WaitAsync(TimeSpan.FromSeconds(1), this.cancellationToken);
+        return task.GetHashCode();
+    }
+
+    /*[Benchmark]
+    public int Task_WhenAny()
+    {
+        var task = Task.WhenAny(this.TestTask(), Task.Delay(1000, this.cancellationToken));
+        return task.GetHashCode();
+    }*/
+
+    [Benchmark]
+    public int Task_Cts()
+    {
+        using var cts = CancellationTokenSource.CreateLinkedTokenSource(this.cancellationToken);
+        cts.CancelAfter(1000);
+        var task = this.TestTask(cts.Token);
+        return task.GetHashCode();
+    }
+}
