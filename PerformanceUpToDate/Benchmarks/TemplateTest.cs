b0777530a4150c792d8bf39b54454a20e1b29f5b
archi-Doc <archidoc422@gmail.com>
2023-12-25 23:03:04 +0900

    .

---
+++ PerformanceUpToDate/Benchmarks/TemplateTest.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using BenchmarkDotNet.Attributes;
+
+namespace PerformanceUpToDate;
+
+[Config(typeof(BenchmarkConfig))]
+public class TemplateTest
+{
+    public TemplateTest()
+    {
+    }
+
+    [Benchmark]
+    public void Test1()
+    {
+    }
+}
