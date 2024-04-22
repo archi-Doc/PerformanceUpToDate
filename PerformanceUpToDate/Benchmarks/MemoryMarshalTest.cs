ed7000851ce452dc6c18f0f06809d6479becf640
archi-Doc <archidoc422@gmail.com>
2024-04-22 12:05:47 +0900

    .

---
+++ PerformanceUpToDate/Benchmarks/MemoryMarshalTest.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Runtime.InteropServices;
+using BenchmarkDotNet.Attributes;
+
+namespace PerformanceUpToDate;
+
+[Config(typeof(BenchmarkConfig))]
+public class MemoryMarshalTest
+{
+    private byte[] byteArray = new byte[32];
+
+    public MemoryMarshalTest()
+    {
+        this.byteArray[0] = 12;
+        this.byteArray[1] = 34;
+        this.byteArray[2] = 56;
+        this.byteArray[3] = 78;
+
+        var a = this.MemoryMarshal_Read();
+        var b = this.BitConverter_ToUInt64();
+    }
+
+    [Benchmark]
+    public ulong MemoryMarshal_Read()
+    {
+        var span = this.byteArray.AsSpan();
+        MemoryMarshal.Read<double>(span);
+        span = span.Slice(8);
+        return MemoryMarshal.Read<ulong>(span);
+    }
+
+    [Benchmark]
+    public ulong BitConverter_ToUInt64()
+    {
+        var span = this.byteArray.AsSpan();
+        BitConverter.ToDouble(span);
+        span = span.Slice(8);
+        return BitConverter.ToUInt64(span);
+    }
+}
--- PerformanceUpToDate/Program.cs
+++ PerformanceUpToDate/Program.cs
@@ -23,12 +23,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        DebugRun<CubicRootTest>();
+        DebugRun<MemoryMarshalTest>();
 
         // var summary = BenchmarkRunner.Run<ByteCopyTest>(); // SwapTest, MemoryAllocationTest, ByteCopyTest
         var switcher = new BenchmarkSwitcher(new[]
 #pragma warning restore SA1515 // Single-line comment should be preceded by blank line
         {
+            typeof(MemoryMarshalTest),
             typeof(CubicRootTest),
             typeof(CancellationTokenTest),
             typeof(StructImplementation),
