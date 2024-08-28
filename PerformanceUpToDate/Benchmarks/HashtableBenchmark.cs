852f06e7e618dd6777dac94e8803a13cc679eee0
archi-Doc <archidoc422@gmail.com>
2024-08-28 16:14:11 +0900

    .

---
+++ PerformanceUpToDate/Benchmarks/HashtableBenchmark.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace PerformanceUpToDate;
+
+[Config(typeof(BenchmarkConfig))]
+public class HashtableBenchmark
+{
+    private readonly uint[] array = [1, 0x74b416b3, 0x4a3ed64a, 0x700214c2, 0xb10baf28, 0x65fd6e51, 0x93ac2ff2, 0x3cc6fbed];
+    private readonly uint a = 0x74b416b3;
+
+    private readonly Arc.Crypto.UInt32Hashtable<uint> hashtable = new();
+    private readonly Dictionary<uint, uint> dictionary = new();
+
+    public HashtableBenchmark()
+    {
+        foreach (var x in array)
+        {
+            this.hashtable.TryAdd(x, x);
+            this.dictionary.Add(x, x);
+        }
+
+        this.hashtable.TryGetValue(a, out var y);
+        this.dictionary.TryGetValue(a, out y);
+    }
+
+    [Benchmark]
+    public uint Hashtable()
+    {
+        this.hashtable.TryGetValue(a, out var y);
+        return y;
+    }
+
+    [Benchmark]
+    public uint Dictionary()
+    {
+        this.dictionary.TryGetValue(a, out var y);
+        return y;
+    }
+
+    [Benchmark]
+    public uint DictionaryLock()
+    {
+        lock (this.dictionary)
+        {
+            this.dictionary.TryGetValue(a, out var y);
+            return y;
+        }
+    }
+}
--- PerformanceUpToDate/Program.cs
+++ PerformanceUpToDate/Program.cs
@@ -23,12 +23,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        DebugRun<NewInstanceTest2>();
+        DebugRun<HashtableBenchmark>();
 
         // var summary = BenchmarkRunner.Run<ByteCopyTest>(); // SwapTest, MemoryAllocationTest, ByteCopyTest
         var switcher = new BenchmarkSwitcher(new[]
 #pragma warning restore SA1515 // Single-line comment should be preceded by blank line
         {
+            typeof(HashtableBenchmark),
             typeof(LargeStructTest),
             typeof(BytePoolTest),
             typeof(SizeOfBenchmark),
