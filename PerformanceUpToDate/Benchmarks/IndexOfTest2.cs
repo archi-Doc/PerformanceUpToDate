38223bf33616e69bb2f98b582cd53f027e30a773
archi-Doc <archidoc422@gmail.com>
2025-04-24 10:02:42 +0900

    .

---
+++ PerformanceUpToDate/Benchmarks/IndexOfTest2.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace PerformanceUpToDate;
+
+[Config(typeof(BenchmarkConfig))]
+public class IndexOfTest2
+{
+    public const string Text = "123456789Hello World123456789Hello World123456789Hello World/#+Hello World";
+    public const char Separator1 = '/';
+    public const char Separator2 = '#';
+    public const char Separator3 = '+';
+
+    public IndexOfTest2()
+    {
+    }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+    }
+
+    [Benchmark]
+    public int IndexOf()
+        => Text.AsSpan().IndexOf(Separator1);
+
+    [Benchmark]
+    public int IndexOf2()
+    {
+        var span = Text.AsSpan();
+        return span.IndexOf(Separator1) + span.IndexOf(Separator2);
+    }
+
+    [Benchmark]
+    public int IndexOfAny2()
+    {
+        var span = Text.AsSpan();
+        return span.IndexOfAny(Separator1, Separator2);
+    }
+
+    [Benchmark]
+    public int IndexOfAny3()
+    {
+        var span = Text.AsSpan();
+        return span.IndexOfAny(Separator1, Separator2, Separator3);
+    }
+
+    [Benchmark]
+    public int Span()
+    {
+        var span = Text.AsSpan();
+        for (var i = 0; i < span.Length; i++)
+        {
+            if (span[i] == Separator1 || span[i] == Separator2 || span[i] == Separator3)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
--- PerformanceUpToDate/Program.cs
+++ PerformanceUpToDate/Program.cs
@@ -23,12 +23,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        DebugRun<NewInstanceTest2>();
+        DebugRun<IndexOfTest2>();
 
         // var summary = BenchmarkRunner.Run<ByteCopyTest>(); // SwapTest, MemoryAllocationTest, ByteCopyTest
         var switcher = new BenchmarkSwitcher(new[]
 #pragma warning restore SA1515 // Single-line comment should be preceded by blank line
         {
+            typeof(IndexOfTest2),
             typeof(ULongTest),
             typeof(DIContainerBenchmark),
             typeof(HashtableBenchmark),
