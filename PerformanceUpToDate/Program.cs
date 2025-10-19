e15c2b8120623a491932775383742d88803bc847
archi-Doc <archidoc422@gmail.com>
2025-10-19 20:05:39 +0900

    .

--- PerformanceUpToDate/Program.cs
+++ PerformanceUpToDate/Program.cs
@@ -23,12 +23,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        DebugRun<LfToCrLfTest>();
+        DebugRun<FrozenDictionaryTest>();
 
         // var summary = BenchmarkRunner.Run<ByteCopyTest>(); // SwapTest, MemoryAllocationTest, ByteCopyTest
         var switcher = new BenchmarkSwitcher(new[]
 #pragma warning restore SA1515 // Single-line comment should be preceded by blank line
         {
+            typeof(FrozenDictionaryTest),
             typeof(LfToCrLfTest),
             typeof(CompoundAssignmentTest),
             typeof(IndexOfTest2),
