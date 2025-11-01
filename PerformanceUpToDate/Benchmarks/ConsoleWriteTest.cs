4e204fda1bf6fd613687316f642c31b158451101
archi-Doc <archidoc422@gmail.com>
2025-11-01 11:30:56 +0900

    .

---
+++ PerformanceUpToDate/Benchmarks/ConsoleWriteTest.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using BenchmarkDotNet.Attributes;
+
+namespace PerformanceUpToDate;
+
+[Config(typeof(BenchmarkConfig))]
+public class ConsoleWriteTest
+{
+    private readonly string testString = "Hello World!";
+    public ConsoleWriteTest()
+    {
+    }
+
+    [Benchmark]
+    public void String()
+    {
+        Console.Write(this.testString);
+    }
+
+    [Benchmark]
+    public void NewString()
+    {
+        var st = new string(this.testString);
+        Console.Write(st);
+    }
+
+    [Benchmark]
+    public void ReadOnlySpan()
+    {
+        var st = this.testString.AsSpan();
+        Console.Out.Write(st);
+    }
+}
--- PerformanceUpToDate/Program.cs
+++ PerformanceUpToDate/Program.cs
@@ -23,12 +23,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        DebugRun<StringJoinTest>();
+        DebugRun<ConsoleWriteTest>();
 
         // var summary = BenchmarkRunner.Run<ByteCopyTest>(); // SwapTest, MemoryAllocationTest, ByteCopyTest
         var switcher = new BenchmarkSwitcher(new[]
 #pragma warning restore SA1515 // Single-line comment should be preceded by blank line
         {
+            typeof(ConsoleWriteTest),
             typeof(StringJoinTest),
             typeof(FrozenDictionaryTest),
             typeof(LfToCrLfTest),
