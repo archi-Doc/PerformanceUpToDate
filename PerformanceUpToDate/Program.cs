2560e2ac7851eb95d64759cfb4c5c0000ed3e593
archi-Doc <archidoc422@gmail.com>
2025-11-16 22:10:17 +0900

    .

--- PerformanceUpToDate/Program.cs
+++ PerformanceUpToDate/Program.cs
@@ -23,12 +23,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        DebugRun<ConcurrentTest2>();
+        DebugRun<CountTripleQuotesTest>();
 
         // var summary = BenchmarkRunner.Run<ByteCopyTest>(); // SwapTest, MemoryAllocationTest, ByteCopyTest
         var switcher = new BenchmarkSwitcher(new[]
 #pragma warning restore SA1515 // Single-line comment should be preceded by blank line
         {
+            typeof(CountTripleQuotesTest),
             typeof(ConcurrentTest2),
             typeof(ConsoleWriteTest),
             typeof(SumSbyteTest),
