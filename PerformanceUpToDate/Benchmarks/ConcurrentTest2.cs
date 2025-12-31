05f57d997738b31990008d6621bde2816cdda6f4
archi-Doc <archidoc422@gmail.com>
2025-12-31 13:13:56 +0900

    .

--- PerformanceUpToDate/Benchmarks/ConcurrentTest2.cs
+++ PerformanceUpToDate/Benchmarks/ConcurrentTest2.cs
@@ -18,14 +18,37 @@ public class ConcurrentTest2
     private Lock lockObject = new();
     private Arc.Collections.CircularQueue<ConsoleKeyInfo> circularQueue = new(1024);
     private ConcurrentQueue<ConsoleKeyInfo> concurrentQueue = new();
+    private ConcurrentQueue<int> concurrentQueue2 = new();
 
     public ConcurrentTest2()
     {
+        this.concurrentQueue2.Enqueue(1);
+        this.concurrentQueue2.Enqueue(2);
+        this.concurrentQueue2.Enqueue(3);
     }
 
     [GlobalSetup]
     public void Setup()
     {
+    }
+
+    [Benchmark]
+    public bool Queue2_EnqueueDequeue()
+    {
+        this.concurrentQueue2.Enqueue(12);
+        return this.concurrentQueue2.TryDequeue(out var x);
+    }
+
+    [Benchmark]
+    public int Queue2_Count()
+    {
+        return this.concurrentQueue2.Count;
+    }
+
+    [Benchmark]
+    public bool Queue2_IsEmpty()
+    {
+        return this.concurrentQueue2.IsEmpty;
     }
 
     [Benchmark]
@@ -53,14 +76,20 @@ public class ConcurrentTest2
     }
 
     [Benchmark]
+    public bool CircularQueue2()
+    {
+        this.circularQueue.TryEnqueue(this.enterKeyInfo);
+        this.circularQueue.TryEnqueue(this.enterKeyInfo);
+        this.circularQueue.TryDequeue(out var item);
+        return this.circularQueue.TryDequeue(out var item2);
+    }
+
+    [Benchmark]
     public bool ConcurrentQueue()
     {
         this.concurrentQueue.Enqueue(this.enterKeyInfo);
-
         this.concurrentQueue.Enqueue(this.enterKeyInfo);
-
         this.concurrentQueue.TryDequeue(out var item);
-
         return this.concurrentQueue.TryDequeue(out var item2);
     }
 }
