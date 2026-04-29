5b7fd5a00c2449093e7e2f410966ce44f3ade37e
archi-Doc <archidoc422@gmail.com>
2026-04-29 17:07:20 +0900

    .

---
+++ PerformanceUpToDate/Benchmarks/CancellationTokenTest2.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System.Threading;
+using Arc.Collections;
+using BenchmarkDotNet.Attributes;
+
+namespace PerformanceUpToDate;
+
+[Config(typeof(BenchmarkConfig))]
+public class CancellationTokenTest2
+{
+    private readonly CancellationTokenSource cts = new();
+    private readonly CancellationTokenSource cts2 = new();
+    private readonly ObjectPool<CancellationTokenSource> ctsPool = new(() => new());
+
+    public CancellationTokenTest2()
+    {
+    }
+
+    [Benchmark]
+    public CancellationTokenSource CreateCts()
+    {
+        return new();
+    }
+
+    [Benchmark]
+    public CancellationTokenSource RentAndReturnCts()
+    {
+        var cts = this.ctsPool.Rent();
+        cts.TryReset();
+        this.ctsPool.Return(cts);
+        return cts;
+    }
+
+    [Benchmark]
+    public CancellationToken GetToken()
+    {
+        return this.cts.Token;
+    }
+
+    [Benchmark]
+    public CancellationTokenSource CreateAndCancelCts()
+    {
+        var cts = new CancellationTokenSource();
+        cts.Cancel();
+        return cts;
+    }
+
+    [Benchmark]
+    public void Cancel()
+    {
+        try
+        {
+            this.cts2.Cancel();
+        }
+        catch
+        {
+        }
+    }
+
+    [Benchmark]
+    public void CheckAndCancel()
+    {
+        if (!this.cts2.IsCancellationRequested)
+        {
+            this.cts2.Cancel();
+        }
+    }
+}
