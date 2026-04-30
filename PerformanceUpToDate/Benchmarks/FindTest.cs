03cae780c52b02e8095fed53fb3ffdfe903581ae
archi-Doc <archidoc422@gmail.com>
2026-04-30 22:12:38 +0900

    .

---
+++ PerformanceUpToDate/Benchmarks/FindTest.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using BenchmarkDotNet.Attributes;
+
+namespace PerformanceUpToDate;
+
+public record class FindTestClass(int Id)
+{
+    public static implicit operator FindTestClass(int id)
+    {
+        return new(id);
+    }
+}
+
+[Config(typeof(BenchmarkConfig))]
+public class FindTest
+{
+    private readonly List<FindTestClass> list = new();
+    private readonly int targetId = 6;
+
+    public FindTest()
+    {
+        this.list = [1, 2, 3, 4, 5, 6, 7, 8,];
+    }
+
+    [Benchmark]
+    public FindTestClass? Find()
+    {
+        return this.list.Find(x => x.Id == this.targetId);
+    }
+
+    [Benchmark]
+    public FindTestClass? ForEach()
+    {
+        foreach (var x in this.list)
+        {
+            if (x.Id == this.targetId)
+            {
+                return x;
+            }
+        }
+
+        return default;
+    }
+
+    [Benchmark]
+    public FindTestClass? Marshal()
+    {
+        var span = CollectionsMarshal.AsSpan(this.list);
+        for (var i = 0; i < span.Length; i++)
+        {
+            var c = span[i];
+            if (c.Id == targetId)
+            {
+                return c;
+            }
+        }
+
+        return null;
+    }
+}
--- PerformanceUpToDate/Program.cs
+++ PerformanceUpToDate/Program.cs
@@ -23,12 +23,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        DebugRun<CancellationTokenTest2>();
+        DebugRun<FindTest>();
 
         // var summary = BenchmarkRunner.Run<ByteCopyTest>(); // SwapTest, MemoryAllocationTest, ByteCopyTest
         var switcher = new BenchmarkSwitcher(new[]
 #pragma warning restore SA1515 // Single-line comment should be preceded by blank line
         {
+            typeof(FindTest),
             typeof(CancellationTokenTest2),
             typeof(QueueTest),
             typeof(FuncTest),
