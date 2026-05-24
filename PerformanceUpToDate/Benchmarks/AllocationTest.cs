cf2cd8ec91014593e71d9c2902de8c0ea36fc951
archi-Doc <archidoc422@gmail.com>
2026-05-24 09:23:50 +0900

    .

--- PerformanceUpToDate/Benchmarks/AllocationTest.cs
+++ PerformanceUpToDate/Benchmarks/AllocationTest.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
+using System;
 using System.Runtime.InteropServices;
 using BenchmarkDotNet.Attributes;
 
@@ -9,9 +10,23 @@ namespace PerformanceUpToDate;
 public class AllocationTest
 {
     private const int Size = 32;
+    private const uint ZeroMemory = 0x00000008;
+
+    private readonly nint heap;
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern nint GetProcessHeap();
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern nint HeapAlloc(nint hHeap, uint dwFlags, nuint dwBytes);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool HeapFree(nint hHeap, uint dwFlags, nint lpMem);
 
     public AllocationTest()
     {
+        this.heap = GetProcessHeap();
     }
 
     [Benchmark]
@@ -33,5 +48,13 @@ public class AllocationTest
     {
         void* p = NativeMemory.Alloc(Size);
         NativeMemory.Free(p);
+    }
+
+    [Benchmark]
+    public nint HeapAlloc()
+    {
+        var p = HeapAlloc(this.heap, 0, Size);
+        HeapFree(heap, 0, p);
+        return p;
     }
 }
