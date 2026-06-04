af3e2f34b3dd80183508a3202745858a563e162d
archi-Doc <archidoc422@gmail.com>
2026-06-04 19:44:44 +0900

    .

--- PerformanceUpToDate/Program.cs
+++ PerformanceUpToDate/Program.cs
@@ -23,12 +23,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        DebugRun<IndexOfTest3>();
+        DebugRun<MultiLineCommentTest>();
 
         // var summary = BenchmarkRunner.Run<ByteCopyTest>(); // SwapTest, MemoryAllocationTest, ByteCopyTest
         var switcher = new BenchmarkSwitcher(new[]
 #pragma warning restore SA1515 // Single-line comment should be preceded by blank line
         {
+            typeof(MultiLineCommentTest),
             typeof(IndexOfTest3),
             typeof(CountLeadingSpacesTest),
             typeof(AllocationTest),
