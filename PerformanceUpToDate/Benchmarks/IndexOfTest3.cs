4eec9ebf765c6066284f13c3d47fedbff6cc8782
archi-Doc <archidoc422@gmail.com>
2026-06-04 15:56:28 +0900

    .

---
+++ PerformanceUpToDate/Benchmarks/IndexOfTest3.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace PerformanceUpToDate;
+
+[Config(typeof(BenchmarkConfig))]
+public class IndexOfTest3
+{
+    public const string Text = "123456789Hello World123456789Hello*World123456789Hello World/#+Hello World";
+    public const char Separator1 = '*';
+    public const char Separator2 = '\n';
+    public const string Separators = "*\n";
+
+    private readonly SearchValues<char> searchValues = SearchValues.Create(Separators);
+
+    public IndexOfTest3()
+    {
+    }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+    }
+
+    [Benchmark]
+    public int IndexOf()
+        => Text.AsSpan().IndexOf(Separator1);
+
+    [Benchmark]
+    public int IndexOfAny()
+        => Text.AsSpan().IndexOfAny(Separator1, Separator2);
+
+    [Benchmark]
+    public int SearchValue()
+        => Text.AsSpan().IndexOfAny(searchValues);
+}
--- PerformanceUpToDate/Program.cs
+++ PerformanceUpToDate/Program.cs
@@ -23,12 +23,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        DebugRun<CountLeadingSpacesTest>();
+        DebugRun<IndexOfTest3>();
 
         // var summary = BenchmarkRunner.Run<ByteCopyTest>(); // SwapTest, MemoryAllocationTest, ByteCopyTest
         var switcher = new BenchmarkSwitcher(new[]
 #pragma warning restore SA1515 // Single-line comment should be preceded by blank line
         {
+            typeof(IndexOfTest3),
             typeof(CountLeadingSpacesTest),
             typeof(AllocationTest),
             typeof(FindTest),
