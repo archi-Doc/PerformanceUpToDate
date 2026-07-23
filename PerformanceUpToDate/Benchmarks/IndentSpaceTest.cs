bf9650bf7dd0852aa94aee11130c3e2564dc1794
archi-Doc <archidoc422@gmail.com>
2026-07-23 23:37:55 +0900

    .

---
+++ PerformanceUpToDate/Benchmarks/IndentSpaceTest.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace PerformanceUpToDate;
+
+[Config(typeof(BenchmarkConfig))]
+public class IndentSpaceTest
+{
+    private readonly char[] spaceBuffer;
+
+    public IndentSpaceTest()
+    {
+        this.spaceBuffer = new char[100];
+        Array.Fill(this.spaceBuffer, ' ');
+    }
+
+    [Benchmark]
+    public StringBuilder AppendRepeat()
+    {
+        var sb = new StringBuilder();
+        sb.Append(' ', 100);
+        return sb;
+    }
+
+    [Benchmark]
+    public StringBuilder AppendBuffer()
+    {
+        var sb = new StringBuilder();
+        sb.Append(this.spaceBuffer);
+        return sb;
+    }
+}
